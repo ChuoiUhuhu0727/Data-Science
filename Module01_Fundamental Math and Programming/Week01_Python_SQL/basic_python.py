24a906d3be62561f9502ec0f188ecd98118973bc
ChuoiUhuhu <168626994+ChuoiUhuhu0727@users.noreply.github.com>
2025-06-06 23:50:47 +0700

    Update basic_python.py

--- Module01_Fundamental Math and Programming/Week01_Python_SQL/basic_python.py
+++ Module01_Fundamental Math and Programming/Week01_Python_SQL/basic_python.py
@@ -50,8 +50,93 @@ def contain(data, value):
   return any(i == value for i in data)
 
 data1 = [1 , 5 , 2 , 91 , -10]
+
+
 # print(contain(data1, 5))
 assert contain(data1, 5) == True
 
 data2 = [2 , 5 , -5 , 10 , 11]
 print(contain(data2, 3))
+
+def compute_precision(TP, FP):
+  return round(TP/(TP + FP), 2)
+
+def compute_recall(TP, FN):
+  return round(TP/(TP+FN), 2)
+
+def compute_f1_score(precision, recall):
+  return round(2 * (precision * recall)/(precision + recall), 2)
+
+def lie_truth(y_hat, y):
+  if (y_hat < 0.5 and y == 0) or (y_hat >= 0.5 and y == 1):
+    r = 1
+  else:
+    r = 0
+  return r
+
+print(lie_truth(0.3, 0))
+print(lie_truth(0.8, 0))
+print(lie_truth(0.7, 0))
+print(lie_truth(0.4, 0))
+
+
+print(lie_truth(0.6, 1))
+print(lie_truth(0.8, 1))
+print(lie_truth(0.9, 1))
+print(lie_truth(0.2, 1))
+
+# Get accustomed to docstring function
+def sum_function(num1, num2):
+  '''
+  Sum of two numbers
+  input: num1, num2
+  output: num1+num2
+  '''
+  return num1 + num1
+
+print(sum_function.__doc__)
+
+def reverse_string(x):
+  res = ""
+  for i in x: 
+    res = i + res
+    print("res here is: ", res)
+  return res
+
+x = "apricot"
+print(reverse_string(x))
+
+# PI Estimation: Gregory-Leibniz Series
+n = 1000
+PI = 0
+for i in range(1, n + 1):
+  PI += ((-1)**(i+1))/(2*i - 1)
+PI = PI*4 
+
+print("Estimated PI is ", PI)
+
+# Calculate area of half circles
+import math
+
+length = 2
+delta = 0.1
+area = 0
+
+num_rectangles = length/delta
+print("num rectangles are: ", num_rectangles)
+
+def get_y(x):
+  return math.sqrt(1-x*x)
+
+x = -1.0
+for _ in range(int(num_rectangles)):
+  # current
+  y = get_y(x)
+  print(x, y)
+
+  area = area + y*delta
+
+  # update
+  x = x + delta
+
+print(area*2)
