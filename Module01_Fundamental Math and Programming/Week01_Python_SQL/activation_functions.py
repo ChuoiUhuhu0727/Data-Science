a0a7d322cf9debf53dd3a970552c6d7c262f5630
ChuoiUhuhu <168626994+ChuoiUhuhu0727@users.noreply.github.com>
2025-06-10 23:36:59 +0700

    Update activation_functions.py

--- Module01_Fundamental Math and Programming/Week01_Python_SQL/activation_functions.py
+++ Module01_Fundamental Math and Programming/Week01_Python_SQL/activation_functions.py
@@ -1,3 +1,44 @@
+# Exercise: Activation Functions
+def evaluate_f1_components(tp: int, fp:int, fn: int):
+  """
+  Compute precision, recall, and F1-score
+  from counts of tp, fp, fn.
+  """
+  try:
+    validate_input(tp, fp, fn)
+    precision, recall, f1_score = compute_f1(tp, fp, fn)
+    print(f"precision is {precision}")
+    print(f"recall is {recall}")
+    print(f"f1-score is {f1_score}")
+  except ValueError as e:
+    print(e)
+
+def validate_input(tp: int, fp: int, fn: int) -> bool:
+  for value, name in zip((tp, fp, fn), ("tp", "fp", "fn")):
+    if not isinstance(value, int):
+      raise ValueError(f"{name} must be int")
+    elif value < 0:
+      raise ValueError(f"{name} must >= 0")
+  return True
+
+def compute_f1(tp: int, fp: int, fn: int) -> tuple[float, float, float]:
+  if ((tp + fp) == 0) or ((tp + fn) == 0):
+    raise ValueError("Dividend cannot be zero")
+  else:
+    precision = tp / (tp + fp)
+    recall = tp / (tp + fn)
+
+  if (precision + recall == 0):
+    raise ValueError("Dividend cannot be zero")
+  else:
+    f1_score = 2 * (precision * recall)/(precision + recall)
+
+  return precision, recall, f1_score
+
+# Test Cases
+evaluate_f1_components(tp=2, fp=3, fn=4)
+evaluate_f1_components(tp="a", fp=3, fn=4)
+
 
 # Exercise 2
 import numpy as np
